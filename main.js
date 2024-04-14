859ee9512e7b891d7145bfaea81f4010878cb930
Nestani123 <nestangagadze96@gmail.com>
2024-04-14 21:52:34 +0400

    added some java

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-let menu = document.querySelector('#menu-icon');
-let navBar = document.querySelector('.nav-bar');
+let menu = document.querySelector("#menu-icon");
+let navBar = document.querySelector(".nav-bar");
+let header = document.querySelector("header");
 
-menu.onClick = ()  => {
-menu.classList.toggle('bx-x');
-navBar.classList.toggle('active');
-}
+menu.addEventListener('click',() => {
+    menu.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+});
+
+
+
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navBar.classList.remove('active');
+};
+
+window.addEventListener('scroll', () =>{
+header.classList.toggle('shadow', window.scrollY > 0);
+});
